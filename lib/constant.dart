18bc3d815176e99470d3daffed377c80fc0dc943
GuntKritamate <kritamate.agis@mail.kmutt.ac.th>
2021-09-12 17:22:21 +0700

    [ADD] Login and product list page
    Front-end implementation but something still missing

---
+++ lib/constant.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kHeadingTextStyle = TextStyle(
+  fontFamily: 'Prompt',
+  fontSize: 30.0,
+);
+
+const kAppbarTextStyle = TextStyle(
+  fontFamily: 'Prompt',
+  fontSize: 25.0,
+);
+
+const kLoginHeadingTextStyle = TextStyle(
+  fontFamily: 'Prompt',
+  fontSize: 30.0,
+  fontWeight: FontWeight.bold,
+);
