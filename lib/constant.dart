a555501f9b8e90202b214a9bc6d47fdcfbf4faff
mineminddd <supapatr.39052@mail.kmutt.ac.th>
2021-09-17 03:49:49 +0700

    [ADD] Color theme

--- lib/constant.dart
+++ lib/constant.dart
@@ -2,16 +2,67 @@ import 'package:flutter/material.dart';
 
 const kHeadingTextStyle = TextStyle(
   fontFamily: 'Prompt',
-  fontSize: 30.0,
+  fontSize: 30.0
 );
 
 const kAppbarTextStyle = TextStyle(
   fontFamily: 'Prompt',
-  fontSize: 25.0,
+  fontSize: 25.0
 );
 
 const kLoginHeadingTextStyle = TextStyle(
   fontFamily: 'Prompt',
   fontSize: 30.0,
-  fontWeight: FontWeight.bold,
+  fontWeight: FontWeight.bold
 );
+
+const kNormalTextStyle = TextStyle(
+  fontFamily: 'Prompt',
+  fontSize: 20.0
+);
+
+const kNormalAlertTextStyle = TextStyle(
+  fontFamily: 'Prompt',
+  fontSize: 20.0,
+  color:deepOrange
+);
+
+const whiteSmallNormalTextStyle = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 18.0,
+    color: deepWhite
+);
+
+const blackSmallNormalTextStyle = TextStyle(
+    fontFamily: 'Prompt',
+    fontSize: 18.0,
+    color: deepBlack
+);
+
+
+//Farm's color
+const Color deepOrange = Color(0xFFE19F00);
+const Color lightOrange = Color(0xFFF3D998);
+//Admin's color
+const Color deepBlue = Color(0xFF395066);
+const Color mediumBlue = Color(0xFF48637D);
+const Color lightBlue = Color(0xFF527E9B);
+//Customer's color
+  // soon
+
+//Status color
+const Color successDeepGreen = Color(0xFF25AF43);
+const Color successGreen = Color(0xFF3ADC5D);
+const Color waitingDeepYellow = Color(0xFFF19710);
+const Color waitingYellow = Color(0xFFF6B26B);
+const Color notYedDeepRed = Color(0xFFF62D2D);
+const Color notYetRed = Color(0xFFFF5959);
+//Text color
+const Color deepBlack = Color(0xFF000000);
+const Color mediumBlack = Color(0xFF50555C);
+const Color lightBlack = Color(0xFF979797);
+const Color deepWhite = Color(0xFFFFFFFF);
+//Background color
+const Color deepGrayBackground = Color(0xFF979797);
+const Color mediumGrayBackground = Color(0xFFE4E6E8);
+const Color lightGrayBackground = Color(0xFFF1EFEF);
